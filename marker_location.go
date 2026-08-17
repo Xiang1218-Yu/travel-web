fd128fd90104f8de79ce6f62a6265debcfc4290a
Xiang1218-Yu <1061366940@qq.com>
2026-08-17 09:59:48 +0800

    fix(gold)：travel-web_0005

--- marker_location.go
+++ marker_location.go
@@ -1,7 +1,7 @@
 package main
 
 func hasMarkerLocation(location Location) bool {
-	return location.Latitude != 0 || location.Longitude != 0
+	return location.Name != "" || location.Latitude != 0 || location.Longitude != 0
 }
 
 func markerCoordinates(location Location) (float64, float64) {
